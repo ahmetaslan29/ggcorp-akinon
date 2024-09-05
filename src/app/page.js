c1a933e8a131335a77035a2b16f62d1e47460759
Alper Aktas <aktasalper0708@gmail.com>
2024-09-05 21:23:16 +0300

    alper : banner added

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import { Application } from "@/component/application";
-import HeroBanner from "@/component/hero-banner";
 import Rules from "./rules/page";
 import TournamentPage from "@/component/rules";
+import HeroBanner from "@/component/home/hero-banner";
+import MediaPromoBlade from "@/component/home/banner";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
     <HeroBanner></HeroBanner>
      <TournamentPage></TournamentPage>
+     <MediaPromoBlade></MediaPromoBlade>
     </main>
   );
 }
